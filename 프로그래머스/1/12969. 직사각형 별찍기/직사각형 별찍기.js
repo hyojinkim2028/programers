00b959653cfe8db3b18f23f461873eecd63d6410
rizzy <134348257+hyojinkim2028@users.noreply.github.com>
2023-11-17 10:01:49 +0900

    [level 1] Title: 직사각형 별찍기, Time: 55.67 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,10 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = '*'.repeat(Number(n[0])), b = Number(n[1]);
+    for (let i = 0 ; i < b ; i++) {
+      console.log(a);
+    }
+    
+    
+});
