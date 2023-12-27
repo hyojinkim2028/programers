7b43a40d3b1b3c36757026fbb072b77ba485e628
rizzy <134348257+hyojinkim2028@users.noreply.github.com>
2023-12-27 10:40:08 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 194.12 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,16 +1,4 @@
 function solution(numbers) {
-  var answer = []
-  for (let i = 0; i < numbers.length; i++) {
-    let strNum = '0' + numbers[i].toString(2)
-    if (numbers[i] % 2 === 0) answer.push(numbers[i] + 1)
-    else {
-      let idx = strNum.lastIndexOf('01')
-      let newNum =
-        strNum.substring(0, idx) +
-        '10' +
-        strNum.substring(idx + 2, strNum.length)
-      answer.push(parseInt(newNum, 2))
-    }
-  }
-  return answer
+    var answer = [];
+    return answer;
 }
